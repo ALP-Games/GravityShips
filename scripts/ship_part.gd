223eb1a81076e6d79adad99c6b370df8153edcad
Andrej Lapin <lapin.andrej1@gmail.com>
2024-07-14 22:49:21 +0300

    improved ship

--- scripts/ship_part.gd
+++ scripts/ship_part.gd
@@ -13,14 +13,14 @@ func _exit_tree():
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	for child in get_children():
-		if child is ShipPart:
-			var joint = PinJoint3D.new()
-			add_child(joint)
-			joint.node_a = self.get_path()
-			joint.node_b = child.get_path()
-			joint.exclude_nodes_from_collision = false
+#func _ready():
+	#for child in get_children():
+		#if child is ShipPart:
+			#var joint = PinJoint3D.new()
+			#add_child(joint)
+			#joint.node_a = self.get_path()
+			#joint.node_b = child.get_path()
+			#joint.exclude_nodes_from_collision = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
