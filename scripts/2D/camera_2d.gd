dad5e1886018548d75ad726c7985667052e8769b
AndrejLapin <lapin.andrej1@gmail.com>
2024-09-04 23:04:45 +0300

    removed jolt, new movement, fps lock, new gravity projectile

--- scripts/2D/camera_2d.gd
+++ scripts/2D/camera_2d.gd
@@ -2,12 +2,7 @@ extends Camera2D
 
 @export var target: Node2D = null
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta: float) -> void:
 	if target:
 		global_position = target.global_position
--- scripts/2D/game_manager.gd
+++ scripts/2D/game_manager.gd
@@ -3,6 +3,10 @@ class_name GameManager extends Node
 @onready var _world_ref: Node2D = get_tree().get_current_scene()
 
 
+func _ready() -> void:
+	# fps lock 
+	Engine.max_fps = 60
+
+
 func get_world_ref() -> Node2D:
 	return _world_ref
-
