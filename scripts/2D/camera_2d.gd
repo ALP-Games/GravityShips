c7a4b5fdb6949f31b5327159ea67817ee73f93ac
pauliens149 <97951822+pauliens149@users.noreply.github.com>
2024-09-15 19:02:12 +0300

    Revert "Merge branch 'andrej'"
    This reverts commit 52efe4706fdebc5eedef6420ad92487393e89fdc, reversing
    changes made to 813479bdbede41b4bd0e92e4ebb63101f6d949de.

--- scripts/2D/camera_2d.gd
+++ scripts/2D/camera_2d.gd
@@ -1,24 +1,13 @@
 extends Camera2D
 
-const BASE_RESOLUTION: Vector2 = Vector2(1920, 1080)
-
 @export var target: Node2D = null
 
-#var _base_zoom: Vector2 = Vector2.ZERO
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
 
 
-func _ready() -> void:
-	var current_resolution := get_viewport_rect().size
-	var height_difference := BASE_RESOLUTION.y / current_resolution.y
-	var new_zoom_value := 1.0 / ((1.0 / zoom.y) * height_difference)
-	zoom = Vector2(new_zoom_value, new_zoom_value)
-
-
-func _physics_process(delta: float) -> void:
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
 	if target:
 		global_position = target.global_position
-
-
-#func _process(delta: float) -> void:
-	#if target:
-		#global_position = target.global_position
