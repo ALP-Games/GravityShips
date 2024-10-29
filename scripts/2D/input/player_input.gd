a425b42ed573f0c20f47a458eeb7b4f76a66c6ce
AndrejLapin <lapin.andrej1@gmail.com>
2024-10-29 22:53:08 +0200

    consistent gun shooting

--- scripts/2D/input/player_input.gd
+++ scripts/2D/input/player_input.gd
@@ -5,7 +5,8 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouse:
 		#_mouse_position = get_global_mouse_position()
 		#look_at(get_global_mouse_position())
-		_shoot_input = event.button_mask == 1 and event.is_pressed()
+		if event.button_mask == 1 and event.is_pressed():
+			_shoot_input = true
 		#if event.button_mask == 1 and event.is_pressed():
 			#
 			#var projectile_instance := projectile_scene.instantiate() as Node2D
@@ -22,9 +23,9 @@ func _process(delta: float) -> void:
 	_rotation_input = Input.get_axis("rot_left", "rot_right")
 
 
-#func _physics_process(delta: float) -> void:
-	#_mouse_position = get_global_mouse_position()
-	#_thrust_input = Input.is_action_pressed("thrust")
-	#_reverse_input = Input.is_action_pressed("reverse")
-	#_stop_input = Input.is_action_pressed("stop")
-	#_rotation_input = Input.get_axis("rot_left", "rot_right")
+func _physics_process(delta: float) -> void:
+	call_deferred("_reset_inputs")
+
+
+func _reset_inputs() -> void:
+	_shoot_input = false
